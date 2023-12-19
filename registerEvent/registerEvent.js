5c591445d2da94046da2dca78ab018e9bdc38e32
Natthanan Wannapha <natthanan.wannapha@gmail.com>
2023-12-19 22:04:32 +0700

    Update code collect data (register page)

--- registerEvent/registerEvent.js
+++ registerEvent/registerEvent.js
@@ -1,14 +1,14 @@
 function submitData(){
     let firstnameDOM = document.querySelector("input[name=firstname]")
     let lastnameDOM = document.querySelector("input[name=lastname]")
-    let genderDOM = document.querySelector("input[name=gender]:checked")
+    let genderDOM = document.querySelector("input[name=gender]")
     let birthdayDOM = document.querySelector("input[name=age]")
     let numbercardDOM = document.querySelector("input[name=number-card-person]")
     let emailDOM = document.querySelector("input[name=email]")
-    let phonDOM = document.querySelector("input[name=phone]")
-    let shirtTypesDOM = document.querySelector("input[name=shirt-type]")
-    let shirtSizesDOM = document.querySelector("input[name=shirt-size]")
-    let typeRunDOM = document.querySelector("input[name=type-run]")
+    let phoneDOM = document.querySelector("input[name=phone]")
+    let shirtTypesDOM = document.getElementById("shirt-type-values")
+    let shirtSizesDOM = document.getElementById("shirt-size-values")
+    let typeRunDOM = document.getElementById("run-type-values")
      
     const dataRegisterEvent = {
         firstname: firstnameDOM.value,
@@ -17,7 +17,7 @@ function submitData(){
         birthday: birthdayDOM.value,
         numbercard: numbercardDOM.value,
         email: emailDOM.value,
-        phon: phonDOM.value,
+        phone: phoneDOM.value,
         shirtType: shirtTypesDOM.value,
         shirtSize: shirtSizesDOM.value,
         typeRun: typeRunDOM.value
@@ -27,5 +27,5 @@ function submitData(){
 }   
 
 function cancelData(){
-
+    
 }
