cef7959a6ae832bd84ea034da6c763209dd8ac6c
Natthanan Wannapha <natthanan.wannapha@gmail.com>
2024-01-01 07:25:47 +0700

    update code register

--- register/register.js
+++ register/register.js
@@ -1,69 +1,133 @@
-// click Input (style border is origin) for data valid (red comeback to origin)
-const clickInput = (elementId) => {
-    document.getElementById(elementId.id).style.border = "1px solid rgb(177, 177, 177)"
+// check value in input.
+const checkValue = (value) => {
+    // if value is not emtry, return true.
+    if(value){
+        return true
+    }else{
+        return false
+    }
 }
 
-// check lenght number card and phone
-const checkLengthNumber = (elementDOM,number) =>{
+// check length number card and phone.
+const checkLengthNumber = (elementDOM,number) => {
     var input = document.getElementById(elementDOM.id);
     if (input.value.length > number) {
         input.value = input.value.slice(0, number); // Truncate to 13 characters
     }
 }
 
-// check all data validation
-const dataValidation = (dataDOM) => {
-    // for check format 
-    let patternEmail = ".+@gmail\.com" 
-
-    // if DOM is not have data. it will set style this input (red) and return 1.
-    if(!dataDOM.value && dataDOM.id != "email"){
-        dataDOM.style.border = "1px solid red"
-        return 1
-    } // if data emtry (for id email) or it isn't match format email and id equal 'email' --> return 1 (error)
-    else if(!dataDOM.value || (!dataDOM.value.match(patternEmail) && dataDOM.id == "email")) {
-        dataDOM.style.border = "1px solid red"
-        return 1
+// onInput (style border is origin) for data valid (red comeback to origin)
+const onInputData = (elementDOM) => {
+    elementDOM = document.getElementById(elementDOM.id)
+    if(checkValue(elementDOM.value)){
+        originalInput(elementDOM)
     }else{
-        // if DOM is have data return 0.
-        return 0
+        actionInput(elementDOM)
     }
+}
+
+// check all data emtry.
+const checkAllDataRegister = (allElement) => {
+    let checkNumberEmpty = 0
+    // username
+    if(!checkValue(allElement.usernameDOM.value)){
+        actionInput(allElement.usernameDOM)
+        checkNumberEmpty += 1
+    }
+    // first password
+    if(!checkValue(allElement.firstpasswordDOM.value)){
+        actionInput(allElement.firstpasswordDOM)
+        checkNumberEmpty += 1
+    }
+    // second password
+    if(!checkValue(allElement.secondpasswordDOM.value)){
+        actionInput(allElement.secondpasswordDOM)
+        checkNumberEmpty += 1
+    }
+    // email
+    if(!checkValue(allElement.emailDOM.value)){
+        actionInput(allElement.emailDOM)
+        checkNumberEmpty += 1
+    }
+    // firstname
+    if(!checkValue(allElement.firstnameDOM.value)){
+        actionInput(allElement.firstnameDOM)
+        checkNumberEmpty += 1
+    }
+    // lastname
+    if(!checkValue(allElement.lastnameDOM.value)){
+        actionInput(allElement.lastnameDOM)
+        checkNumberEmpty += 1
+    }
+    // gender
+    if(!checkValue(allElement.genderDOM.value)){
+        actionInput(allElement.genderDOM)
+        checkNumberEmpty += 1
+    }
+    // birthday
+    if(!checkValue(allElement.birthdayDOM.value)){
+        actionInput(allElement.birthdayDOM)
+        checkNumberEmpty += 1
+    }
+    // number card
+    if(!checkValue(allElement.numbercardDOM.value)){
+        actionInput(allElement.numbercardDOM)
+        checkNumberEmpty += 1
+    }
+    // phone
+    if(!checkValue(allElement.phoneDOM.value)){
+        actionInput(allElement.phoneDOM)
+        checkNumberEmpty += 1
+    }
+    return checkNumberEmpty == 0
+}
+
+// check all format data.
+const checkFormatValue = (allElement) => {
+    let checkNumberNotMatchPattern = 0
+    // email
+    if(!allElement.emailDOM.value.match(allElement.emailDOM.pattern)){
+        actionInput(allElement.emailDOM)
+        checkNumberNotMatchPattern += 1
+    }
+    return checkNumberNotMatchPattern == 0
+}
+
+// original input
+const originalInput = (elementDOM) => {
+    elementDOM.style.border = "1px solid rgb(177, 177, 177)"
+}
+
+// action input
+const actionInput = (elementDOM) => {
+    elementDOM.style.border = "1px solid red"
 }
 
 // Get data register user.
 const getRegisterData = async() => {
-    let usernameDOM = document.querySelector("input[name=username]")
-    let firstpasswordDOM = document.querySelector("input[name=password-1]")
-    let secondpasswordDOM = document.querySelector("input[name=password-2]")
-    let emailDOM = document.querySelector("input[name=email]")
-    let firstnameDOM = document.querySelector("input[name=firstname]")
-    let lastnameDOM = document.querySelector("input[name=lastname]")
-    let genderDOM = document.getElementById("gender")
-    let birthdayDOM = document.querySelector("input[name=birthday]")
-    let numbercardDOM = document.querySelector("input[name=number-card]")
-    let phoneDOM = document.querySelector("input[name=phone]")
 
-    // variable check data valid and message tell user.
-    let checkError = 0
-    let messageDOM = document.getElementById("message")
+    dataRegister = {
+        usernameDOM: document.querySelector("input[name=username]"),
+        firstpasswordDOM: document.querySelector("input[name=password-1]"),
+        secondpasswordDOM: document.querySelector("input[name=password-2]"),
+        emailDOM: document.querySelector("input[name=email]"),
+        firstnameDOM: document.querySelector("input[name=firstname]"),
+        lastnameDOM: document.querySelector("input[name=lastname]"),
+        genderDOM: document.getElementById("gender"),
+        birthdayDOM: document.querySelector("input[name=birthday]"),
+        numbercardDOM: document.querySelector("input[name=number-card]"),
+        phoneDOM: document.querySelector("input[name=phone]")
+    }
 
-    // Check all data valid in form. (if it have number more 1 data invalid)
-    checkError += dataValidation(usernameDOM)
-    checkError += dataValidation(firstpasswordDOM)
-    checkError += dataValidation(secondpasswordDOM)
-    checkError += dataValidation(emailDOM)
-    checkError += dataValidation(firstnameDOM)
-    checkError == dataValidation(lastnameDOM)
-    checkError += dataValidation(genderDOM)
-    checkError += dataValidation(birthdayDOM)
-    checkError += dataValidation(numbercardDOM)
-    checkError += dataValidation(phoneDOM)
-    // Check format email
-    checkError += dataValidation(emailDOM)
-    
-    // Validation form
-    if(checkError==0){
-        // It is not have error for input data (full data in form).
+    // check all data valid in form. (if it have number more 1 data invalid)
+    let checkErrorEmtryData = checkAllDataRegister(dataRegister)
+    // check format data
+    let checkErrorFormatData = checkFormatValue(dataRegister)
+    // combine together
+    let checkError = checkErrorEmtryData && checkErrorFormatData
+
+    // data register is ready send to database.
+    if(checkError){
         // Collect data
         let dataRegister = {
             username: usernameDOM.value,
@@ -78,16 +142,12 @@ const getRegisterData = async() => {
             phone: phoneDOM.value
         }
 
-        // Register data
         try{
             const response = await axios.post(
                 'http://localhost:8000/user/register',
                 dataRegister
             )
             console.log("ลงทะเบียนสำเร็จ")
-            // Show message success
-            messageDOM.innerText = "ลงทะเบียนสำเร็จ"
-            messageDOM.className = "message success"
 
         } catch(error){
             console.log(error)
@@ -95,13 +155,7 @@ const getRegisterData = async() => {
         
     }else{
         // Data invalid
-        // Show message unsuccess.
-        let errorDOM = "กรอกข้อมูลไม่ครบถ้วน!"
         console.log("ลงทะเบียนไม่สำเร็จ")
-
-        // insert text error to html.
-        messageDOM.innerHTML = errorDOM
-        messageDOM.className = "message unsuccess"
     }
     
 }
