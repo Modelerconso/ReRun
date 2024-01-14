ee6acc89271e857bb37db6da0a3789af89a52cea
Natthanan Wannapha <natthanan.wannapha@gmail.com>
2024-01-15 01:20:32 +0700

    update responsive register event page

--- registerEvent/registerEvent.js
+++ registerEvent/registerEvent.js
@@ -31,3 +31,16 @@ function clearData(){
     document.getElementById("shirt-size-values").value = ""
     document.getElementById("run-type-values").value = ""
 }
+
+// Responsive menu
+const menuResponsive = () => {
+    let menuDOM = document.getElementById("mymenu")
+    let headerDOM = document.getElementById("header")
+    if(menuDOM.className === "menu"){
+        menuDOM.className += " responsive"
+        headerDOM.className += " responsive"
+    }else {
+        menuDOM.className = "menu"
+        headerDOM.className = "header"
+    }
+}
